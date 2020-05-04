498546d4169e6e0caa603e2e6e075fe866faf24d
OpiumSmoke <4507052+OpiumSmoke@users.noreply.github.com>
2020-05-04 16:27:23 +0200

    LH cursors are there

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ else
 fi
 
 cp -r dist/ $DEST_DIR/Layan-cursors
+cp -r dist-lh/ $DEST_DIR/Layan-cursors-LH
 cp -r dist-border/ $DEST_DIR/Layan-border-cursors
 cp -r dist-white/ $DEST_DIR/Layan-white-cursors
 
